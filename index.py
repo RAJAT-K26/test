5ed0f8fdaf83bd2a7da002801506509d299d495b
RAJAT-K26 <97009707+RAJAT-K26@users.noreply.github.com>
2025-02-10 10:48:40 +0530

    Update index.py

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "test file success yes ennnnn!"
+    return "test file success new trigger!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
